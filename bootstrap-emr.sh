5233900d079674c4176754e1f429a844dea66348
Florian-BOUDON <97687795+Florian-BOUDON@users.noreply.github.com>
2023-08-08 16:49:27 +0200

    Add files via upload

---
+++ bootstrap-emr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install wheel==0.38.4
+sudo python3 -m pip install Pillow==9.4.0
+sudo python3 -m pip install pandas==1.5.3
+sudo python3 -m pip install numpy==1.23.5
+sudo python3 -m pip install pyarrow==12.0.1
+sudo python3 -m pip install boto3 
+sudo python3 -m pip install s3fs 
+sudo python3 -m pip install fsspec
+sudo python3 -m pip install boto3
+sudo python3 -m pip install findspark==2.0.1
+sudo python3 -m pip install py4j==0.10.9.7
